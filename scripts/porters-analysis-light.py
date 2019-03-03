8782d29b5ed9a004db30422567cf954b5575aa64
Daniel Scott <info@synapseproduction.com>
2019-03-03 02:51:10 -0700

    Set dataset paths as relative.

--- scripts/porters-analysis-light.py
+++ scripts/porters-analysis-light.py
@@ -1,47 +1,37 @@
 
 # coding: utf-8
 
-# In[1]:
+'''
+Copyright Daniel Scott 2019
 
+Background:
+MBA students will often be giving excercises to prepare them for dynamic demands
+in moden strategic corporate planning. Some well respected simulators - such as
+Globestrat - are strong at providing students with uncertainty, but offer little
+to no explanation of how metrics are ordered in bottom influence.
+
+Intent:
+The script acts as a framework to extract the rank order of available metrics,
+and then apply a profit-maximing function to help guide strategy for the user.
+The output is a list of recommended decisions to maximize profit while minimizing expense.
+
+Method:
+We apply the Nonlinear conjugate gradient method to extract coefficients which
+represent the least error from each term, and then Broyden–Fletcher–Goldfarb–Shanno
+algorithm to minimize each cost term from the objective function, and output
+the optimized solutions.
+
+Disclaimer:
+This is meant for educational purposes only and isn't intended to cause
+harm to business simulators. Users use it at their own risk. By using this
+script, you are agreeing to claim all responsibility for mis-use.
+
+'''
 
 import numpy as np
 from scipy.optimize import minimize
 import pandas as pd
 
-
-# In[110]:
-
-
-'''
-
-Copyright Daniel Scott
-
-Background:
-MBA students will often be giving excercises to prepare them for dynamic demands
-in moden strategic corporate planning. Some well respected simulators - such as 
-Globestrat - are strong at providing students with uncertainty, but offer little
-to no explanation of how metrics are ordered in bottom influence. 
-
-Intent:
-The script acts as a framework to extract the rank order of available metrics, 
-and then apply a profit-maximing function to help guide strategy for the user.
-The output is a list of recommended decisions to maximize profit while minimizing expense.
-
-Method:
-We apply the Nonlinear conjugate gradient method to extract coefficients which 
-represent the least error from each term, and then Broyden–Fletcher–Goldfarb–Shanno 
-algorithm to minimize each cost term from the objective function, providing a 
-vectorized solution.
-
-Disclaimer: 
-This is meant for educational purposes only and isn't intended to cause
-harm to business simulation software. Users use it at their own risk. By using this 
-script, you are agreeing to claim all responsibility for mis-use.
-
-The error rate for predicting sales volume is +/- 5%
-
-
-'''
 
 class Model:
     '''
@@ -49,14 +39,14 @@ class Model:
     '''
 
     def predict(self,X):
-        
+
         X_scaled = self.scaler.transform(X)
         result = self.model.predict(X_scaled)
         return result
-    
-    
+
+
     def fit(self):
-        
+
         import seaborn as sns; sns.set()
         import matplotlib.pyplot as plt
         import pandas as pd
@@ -69,12 +59,13 @@ class Model:
         from sklearn.decomposition import PCA
         from sklearn.feature_selection import RFE
         import math
-        
-        df = pd.read_csv('C:/Users/Dan/Downloads/BUSN450 FEATURES - cv_predict (12).csv')
+
+        # Select Dataset Here
+        df = pd.read_csv('../data/example-dataset.csv')
 
         df.drop(columns=['att_storage','att_comm','att_image','Market Share','csr budget','cumulative growth volume','Small Operations','wages'],inplace=True)
-        
-        
+
+
         # Remove  certain samples from df
         #df = df[df['csr budget']<30]
         #df = df[:-1]
@@ -111,10 +102,10 @@ class Model:
         reg = LinearRegression().fit(X_scaled, y)
 
         clf = LinearRegression()
-        
+
         clf.fit(X_scaled,y)
 
-        
+
         print(reg.score(X_scaled, y))
 
         rfe = RFE(estimator=clf, n_features_to_select=1, step=1)
@@ -147,87 +138,87 @@ class Model:
 
         self.model = reg
         self.scaler = scaler
-        
+
         return self
 
-        
-    
+
+
 
 class Optimizer:
     '''
     Used to store boundaries, and sensitivities entered from the use.
     '''
-    
+
     def __init__(self):
-        
+
         # Bounds - Held Constant
-        
+
         self.agency_base = 6
         self.agency_max = 8
         self.agency_marginal_cost = -500
-        
+
         self.qualIndex_base = 100
         self.qualIndex_max = 120
         self.qualIndex_marginal_cost = -500
-        
+
         self.ecolIndex_base = 100
         self.ecolIndex_max = 120
         self.ecolIndex_marginal_cost = -500
-        
+
         self.price_base = 1
         self.price_max = 1700
         self.price_marginal_cost = 1
-        
+
         self.ad_base = 1
         self.ad_max = 3100
         self.ad_marginal_cost = -1
-        
+
         self.iftech1_base = 0
         self.iftech1_max = 1
         self.iftech1_marginal_cost = -1500
-        
+
         self.iftech2_base = 0
         self.iftech2_max = 1
         self.iftech2_marginal_cost = -1500
-        
+
         self.iftech3_base = 0
         self.iftech3_max = 1
         self.iftech3_marginal_cost = -1500
-        
+
         self.trainBudget_base = 0
         self.trainBudget_max = 1500
         self.trainBudget_marginal_cost = -1
-        
+
         self.csrBudget_base = 0
         self.csrBudget_max = 5
         self.csrBudget_marginal_cost = -350
-        
-        
-        
+
+
+
         # Set bounds for each term
         self.bounds = (    (self.agency_base,self.agency_max),
                            (self.qualIndex_base,self.qualIndex_max),
                            (self.price_base,self.price_max),
                            (self.ad_base,self.ad_max)
-        
+
                       )
-        
-       
+
+
 
         self.con1 = {'type': 'ineq','fun': budget_constraint }
         self.cons = [self.con1]
-        
-        
+
+
         # Estimate constants
-        
+
         # In a future version, constants will use a linear regression estimator.
         self.baseTeamSize = 150
         self.baseAverageWage = 65
         self.baseAgencyTeamSize = 5
-        
-        
-        
-    
+
+
+
+
     def get_surveys(self,sector):
         '''
         Sector may be 'SME, Consumers, or Enterprise'
@@ -250,41 +241,41 @@ class Optimizer:
         self.small_ops = 34
         self.budget = 40000
         return self
-    
-    
-        
+
+
+
     def init_solver_coefs(self):
         '''
         Initialize the solver values
-        
+
         x[0] Agencies
         x[1] Quality index
         x[2] Sector price
         x[3] Geographic-zone advertising
 
         '''
-        
-        
-        
+
+
+
         self.coefs = np.array([    6,
                                    120,
                                    1300,
                                    1700
                                   ])
-        
-        
+
+
         return self
-    
+
 def budget_constraint(x):
     '''
     This constraint tracks the budget and makes sure decisions do not exceed the requested limit.
     '''
-    
-    
-    x0 = x[0] 
+
+
+    x0 = x[0]
     x1 = x[1]
-    x2 = x[2] 
-    x3 = x[3] 
+    x2 = x[2]
+    x3 = x[3]
 
 
     '''
@@ -292,45 +283,45 @@ def budget_constraint(x):
         x[1] Quality index
         x[2] Sector price
         x[3] Geographic-zone advertising
-  
+
     '''
-    
+
     # Team Size Estimation
     team_size = optimizer.baseTeamSize + (x0 * optimizer.baseAgencyTeamSize)
     total_wages = optimizer.baseAverageWage * team_size
- 
-    
+
+
     # Start Expenses --------------------------------
     totalExpense = 0
-    
+
     # Agency Related Expense (Team Growth)
     totalExpense += (x0 * optimizer.agency_marginal_cost)
-    
+
     totalExpense += (x1 * optimizer.qualIndex_marginal_cost)
-  
-    
+
+
     # Advertising Budget
     totalExpense += (x3*.1)
-    
+
     totalExpense += (total_wages *-1)
     totalExpense= abs(totalExpense) / 3
-        
+
     # End Expenses --------------------------------
 
-    
+
     # syntax for less-than inequality
     return  totalExpense - optimizer.budget
-    
-        
+
+
 def objective(x):
     '''
     Objective function to minimize cost/profit ratio. Considers revenues/costs
     associated with each decision.
     '''
-    x0 = x[0] 
+    x0 = x[0]
     x1 = x[1]
-    x2 = x[2] 
-    x3 = x[3] 
+    x2 = x[2]
+    x3 = x[3]
 
     '''
     x[0] Agencies
@@ -338,13 +329,13 @@ def objective(x):
     x[2] Sector price
     x[3] Geographic-zone advertising
     '''
-    
+
     # Team Size Estimation
     team_size = optimizer.baseTeamSize + (x0 * optimizer.baseAgencyTeamSize)
     total_wages = optimizer.baseAverageWage * team_size
-    
-    
-    
+
+
+
     # Start Create Terms-----------------------------
     sector_price = x2
     price_compete = (optimizer.competitor_expected_price / x2 ) * optimizer.sens_price
@@ -355,40 +346,40 @@ def objective(x):
     market_share = optimizer.market_share
     small_operations = optimizer.small_ops
     competition_included = optimizer.competitor_expected_true
-    
+
     # End Create Terms--------------------------------
-    
-    
+
+
     # Start Expenses --------------------------------
     totalExpense = 0
-    
+
     # Agency Related Expense (Team Growth)
     totalExpense += (x0 * optimizer.agency_marginal_cost)
-    
+
     totalExpense += (x1 * optimizer.qualIndex_marginal_cost)
-    
+
     # Advertising Budget
     totalExpense += (x3*.1)
-    
+
     totalExpense += (total_wages *-1)
-        
+
     # End Expenses --------------------------------
-    
-    
-    
+
+
+
     # Start Revenues ------------------------------
     # Sales Volume Estimator
 
-    
+
     '''
     x[0] Agencies
     x[1] Quality index
     x[2] Sector price
-    x[3] Geographic-zone advertising  
+    x[3] Geographic-zone advertising
     '''
-    
+
     X = np.array([[
-        
+
         sector_price,
         price_compete,
         quality,
@@ -404,25 +395,25 @@ def objective(x):
         competition_included,
         #cumulative_growth_volume,
         #csr_budget
-        
+
     ]])
 
 
-   
-    
+
+
     totalVolume = model.predict(X)
 
-    
+
     # Volume * Product Price
     totalRevenue = (totalVolume * x2) / 1000
     # End Revenues ------------------------------
-    
+
     totalExpense= abs(totalExpense) / 3
-    
+
     objective_to_minimize = 1 / (totalRevenue-(totalExpense))
-    
+
     print("Revenue:",totalRevenue,"Expense",totalExpense,'wages',total_wages, 'R-E',(totalRevenue-totalExpense),'objective',objective_to_minimize)
-    
+
     return objective_to_minimize
 
 
@@ -438,16 +429,15 @@ x = [optimizer.coefs[i] for i in range(len(optimizer.coefs))]
 
 
 # objective,initial guesses,method, bounds, constraints
-sol = minimize(objective,x,method='SLSQP',              bounds=optimizer.bounds,constraints=optimizer.cons) 
+sol = minimize(objective,x,method='SLSQP',              bounds=optimizer.bounds,constraints=optimizer.cons)
 
 decisions = np.array(['Agencies',
                     'Quality index',
                     'Sector price',
-                    'Geographic-zone advertising']) 
+                    'Geographic-zone advertising'])
 
 df = pd.DataFrame()
 df['Decision'] = decisions
 df['value'] = sol.x
 df['value'] = df['value'].astype('int')
 df
-
--- scripts/porters-analysis.py
+++ scripts/porters-analysis.py
@@ -1,47 +1,36 @@
 
 # coding: utf-8
 
-# In[1]:
-
-
-import numpy as np
-from scipy.optimize import minimize
-import pandas as pd
-
-
-# In[86]:
-
-
 '''
-
-Copyright Daniel Scott 
+Copyright Daniel Scott 2019
 
 Background:
 MBA students will often be giving excercises to prepare them for dynamic demands
-in moden strategic corporate planning. Some well respected simulators - such as 
+in moden strategic corporate planning. Some well respected simulators - such as
 Globestrat - are strong at providing students with uncertainty, but offer little
-to no explanation of how metrics are ordered in bottom influence. 
+to no explanation of how metrics are ordered in bottom influence.
 
 Intent:
-The script acts as a framework to extract the rank order of available metrics, 
+The script acts as a framework to extract the rank order of available metrics,
 and then apply a profit-maximing function to help guide strategy for the user.
 The output is a list of recommended decisions to maximize profit while minimizing expense.
 
 Method:
-We apply the Nonlinear conjugate gradient method to extract coefficients which 
-represent the least error from each term, and then Broyden–Fletcher–Goldfarb–Shanno 
-algorithm to minimize each cost term from the objective function, providing a 
-vectorized solution.
+We apply the Nonlinear conjugate gradient method to extract coefficients which
+represent the least error from each term, and then Broyden–Fletcher–Goldfarb–Shanno
+algorithm to minimize each cost term from the objective function, and output
+the optimized solutions.
 
-Disclaimer: 
+Disclaimer:
 This is meant for educational purposes only and isn't intended to cause
-harm to business simulation software. Users use it at their own risk. By using this 
+harm to business simulators. Users use it at their own risk. By using this
 script, you are agreeing to claim all responsibility for mis-use.
 
-The error rate for predicting sales volume is +/- 5%
-
-
 '''
+
+import numpy as np
+from scipy.optimize import minimize
+import pandas as pd
 
 class Model:
     '''
@@ -49,14 +38,14 @@ class Model:
     '''
 
     def predict(self,X):
-        
+
         X_scaled = self.scaler.transform(X)
         result = self.model.predict(X_scaled)
         return result
-    
-    
+
+
     def fit(self):
-        
+
         import seaborn as sns; sns.set()
         import matplotlib.pyplot as plt
         import pandas as pd
@@ -69,12 +58,13 @@ class Model:
         from sklearn.decomposition import PCA
         from sklearn.feature_selection import RFE
         import math
-        
-        df = pd.read_csv('C:/Users/Dan/Downloads/BUSN450 FEATURES - cv_predict (12).csv')
+
+        # Select Dataset Here
+        df = pd.read_csv('../data/example-dataset.csv')
 
         df.drop(columns=['att_storage','att_comm','att_image','Market Share','csr budget','cumulative growth volume','Small Operations','wages'],inplace=True)
-        
-        
+
+
         # Remove  certain samples from df
         #df = df[df['csr budget']<30]
         #df = df[:-1]
@@ -111,10 +101,10 @@ class Model:
         reg = linear_model.Lasso(alpha=0.05).fit(X_scaled, y)
 
         clf = linear_model.Lasso(alpha=0.05)
-        
+
         clf.fit(X_scaled,y)
 
-        
+
         print(reg.score(X_scaled, y))
 
         rfe = RFE(estimator=clf, n_features_to_select=1, step=1)
@@ -147,63 +137,63 @@ class Model:
 
         self.model = reg
         self.scaler = scaler
-        
+
         return self
 
-        
-    
+
+
 
 class Optimizer:
     '''
     Used to store boundaries, and sensitivities entered from the use.
     '''
-    
+
     def __init__(self):
-        
+
         # Bounds - Held Constant
-        
+
         self.agency_base = 6
         self.agency_max = 8
         self.agency_marginal_cost = -500
-        
+
         self.qualIndex_base = 100
         self.qualIndex_max = 180
         self.qualIndex_marginal_cost = -300
-        
+
         self.ecolIndex_base = 100
         self.ecolIndex_max = 180
         self.ecolIndex_marginal_cost = -300
-        
+
         self.price_base = 1
         self.price_max = 1700
         self.price_marginal_cost = 1
-        
+
         self.ad_base = 1
         self.ad_max = 3100
         self.ad_marginal_cost = -1
-        
+
         self.iftech1_base = 0
         self.iftech1_max = 1
         self.iftech1_marginal_cost = -1500
-        
+
         self.iftech2_base = 0
         self.iftech2_max = 1
         self.iftech2_marginal_cost = -1500
-        
+
         self.iftech3_base = 0
         self.iftech3_max = 1
         self.iftech3_marginal_cost = -1500
-        
+
         self.trainBudget_base = 0
         self.trainBudget_max = 1500
         self.trainBudget_marginal_cost = -1
-        
+
         self.csrBudget_base = 0
         self.csrBudget_max = 5
         self.csrBudget_marginal_cost = -350
-        
-        
-        
+
+
+
         # Set bounds for each term
         self.bounds = (    (self.agency_base,self.agency_max),
                            (self.qualIndex_base,self.qualIndex_max),
@@ -216,23 +206,23 @@ class Optimizer:
                            (self.trainBudget_base,self.trainBudget_max),
                            (self.csrBudget_base,self.csrBudget_max)
                       )
-        
-       
+
+
 
         self.con1 = {'type': 'ineq','fun': budget_constraint }
         self.cons = [self.con1]
-        
-        
+
+
         # Estimate constants
-        
+
         # In a future version, constants will use a linear regression estimator.
         self.baseTeamSize = 150
         self.baseAverageWage = 65
         self.baseAgencyTeamSize = 5
-        
-        
-        
-    
+
+
+
+
     def get_surveys(self,sector):
         '''
         Sector may be 'SME, Consumers, or Enterprise'
@@ -255,13 +245,13 @@ class Optimizer:
         self.small_ops = 34
         self.budget = 40000
         return self
-    
-    
-        
+
+
+
     def init_solver_coefs(self):
         '''
         Initialize the solver values
-        
+
         x[0] Agencies
         x[1] Quality index
         x[2] Ecology index
@@ -273,9 +263,9 @@ class Optimizer:
         x[8] Training Budget
         x[9] CSR Budget
         '''
-        
-        
-        
+
+
+
         self.coefs = np.array([    6,
                                    120,
                                    101,
@@ -287,22 +277,22 @@ class Optimizer:
                                    110,
                                    2
                                   ])
-        
-        
+
+
         return self
-    
+
 def budget_constraint(x):
     '''
     This constraint tracks the budget and makes sure decisions do not exceed the requested limit.
     '''
-    
-    
-    x0 = x[0] 
+
+
+    x0 = x[0]
     x1 = x[1]
-    x2 = x[2] 
-    x3 = x[3] 
+    x2 = x[2]
+    x3 = x[3]
     x4 = x[4]
-    x5 = x[5] 
+    x5 = x[5]
     x6 = x[6]
     x7 = x[7]
     x8 = x[8]
@@ -318,61 +308,61 @@ def budget_constraint(x):
     x[6] ifTech2
     x[7] ifTech3
     x[8] Training Budget
-    x[9] CSR Budget    
+    x[9] CSR Budget
     '''
-    
+
     # Team Size Estimation
     team_size = optimizer.baseTeamSize + (x0 * optimizer.baseAgencyTeamSize)
     total_wages = optimizer.baseAverageWage * team_size
- 
-    
+
+
     # Start Expenses --------------------------------
     totalExpense = 0
-    
+
     # Agency Related Expense (Team Growth)
     totalExpense += (x0 * optimizer.agency_marginal_cost)
-    
+
     totalExpense += (x1 * optimizer.qualIndex_marginal_cost)
     totalExpense += (x2 * optimizer.ecolIndex_marginal_cost)
-    
+
     # Advertising Budget
     totalExpense += (x4)
-    
+
     # Technology Investment
     totalExpense += (x5 * optimizer.iftech1_marginal_cost)
     totalExpense += (x6 * optimizer.iftech2_marginal_cost)
     totalExpense += (x7 * optimizer.iftech3_marginal_cost)
 
     totalExpense += (total_wages *-1)
-    
-  
-    
+
+
+
     # Training Costs
     totalExpense += x8
-    
+
     # CSR Costs
     totalExpense += x9
-        
+
     totalExpense= abs(totalExpense) / 3
-        
+
     # End Expenses --------------------------------
 
-    
+
     # syntax for less-than inequality
     return  totalExpense - optimizer.budget
-    
-        
+
+
 def objective(x):
     '''
     Objective function to minimize cost/profit ratio. Considers revenues/costs
     associated with each decision.
     '''
-    x0 = x[0] 
+    x0 = x[0]
     x1 = x[1]
-    x2 = x[2] 
-    x3 = x[3] 
+    x2 = x[2]
+    x3 = x[3]
     x4 = x[4]
-    x5 = x[5] 
+    x5 = x[5]
     x6 = x[6]
     x7 = x[7]
     x8 = x[8]
@@ -388,15 +378,15 @@ def objective(x):
     x[6] ifTech2
     x[7] ifTech3
     x[8] Training Budget
-    x[9] CSR Budget    
+    x[9] CSR Budget
     '''
-    
+
     # Team Size Estimation
     team_size = optimizer.baseTeamSize + (x0 * optimizer.baseAgencyTeamSize)
     total_wages = optimizer.baseAverageWage * team_size
-    
-    
-    
+
+
+
     # Start Create Terms-----------------------------
     sector_price = x3
     price_compete = (optimizer.competitor_expected_price / x3 ) * optimizer.sens_price
@@ -413,44 +403,44 @@ def objective(x):
     competition_included = optimizer.competitor_expected_true
     cumulative_growth_volume = optimizer.cumul_growth_volume
     csr_budget = x9
-    
+
     # End Create Terms--------------------------------
-    
-    
+
+
     # Start Expenses --------------------------------
     totalExpense = 0
-    
+
     # Agency Related Expense (Team Growth)
     totalExpense += (x0 * optimizer.agency_marginal_cost)
-    
+
     totalExpense += (x1 * optimizer.qualIndex_marginal_cost)
     totalExpense += (x2 * optimizer.ecolIndex_marginal_cost)
-    
+
 
     # Advertising Budget
     totalExpense += (x4)
-    
+
     # Technology Investment
     totalExpense += (x5 * optimizer.iftech1_marginal_cost)
     totalExpense += (x6 * optimizer.iftech2_marginal_cost)
     totalExpense += (x7 * optimizer.iftech3_marginal_cost)
 
     totalExpense += (total_wages *-1)
-    
+
     # Training Costs
     totalExpense += x8
-    
+
     # CSR Costs
     totalExpense += x9
-        
+
     # End Expenses --------------------------------
-    
-    
-    
+
+
+
     # Start Revenues ------------------------------
     # Sales Volume Estimator
 
-    
+
     '''
     x[0] Agencies
     x[1] Quality index
@@ -461,11 +451,11 @@ def objective(x):
     x[6] ifTech2
     x[7] ifTech3
     x[8] Training Budget
-    x[9] CSR Budget    
+    x[9] CSR Budget
     '''
-    
+
     X = np.array([[
-        
+
         sector_price,
         price_compete,
         quality,
@@ -481,25 +471,25 @@ def objective(x):
         competition_included,
         #cumulative_growth_volume,
         #csr_budget
-        
+
     ]])
 
 
-   
-    
+
+
     totalVolume = model.predict(X)
 
-    
+
     # Volume * Product Price
     totalRevenue = (totalVolume * x3) / 1000
     # End Revenues ------------------------------
-    
+
     totalExpense= abs(totalExpense) / 3
-    
+
     objective_to_minimize = 1 / (totalRevenue-(totalExpense))
-    
+
     print("Revenue:",totalRevenue,"Expense",totalExpense,'wages',total_wages, 'R-E',(totalRevenue-totalExpense))
-    
+
     return objective_to_minimize
 
 
@@ -516,7 +506,7 @@ x = [optimizer.coefs[i] for i in range(len(optimizer.coefs))]
 
 
 # objective,initial guesses,method, bounds, constraints
-sol = minimize(objective,x,method='SLSQP',              bounds=optimizer.bounds,constraints=optimizer.cons) 
+sol = minimize(objective,x,method='SLSQP',              bounds=optimizer.bounds,constraints=optimizer.cons)
 
 decisions = np.array(['Agencies',
                     'Quality index',
@@ -527,11 +517,10 @@ decisions = np.array(['Agencies',
                     'ifTech2',
                     'ifTech3',
                     'Training Budget',
-                    'CSR Budget']) 
+                    'CSR Budget'])
 
 df = pd.DataFrame()
 df['Decision'] = decisions
 df['value'] = sol.x
 df['value'] = df['value'].astype('int')
 df
-
